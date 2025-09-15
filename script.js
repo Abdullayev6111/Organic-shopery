b2524197f83801c6b7776daad5de7ed66f9e4ee1
Abdurauf Abdullayev <abdullayevaxmadullo977@gmail.com>
2025-09-15 21:16:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// array-1
+
+// const arr1 = [1, 2];
+// const arr2 = [3, 4];
+
+// const mixedArray = arr1.concat(arr2);
+// console.log(mixedArray);
+
+// array-2
+
+// const arr = [3, 1, 4, 2];
+// console.log(arr.sort((a, b) => a - b));
+
+// array-3
+
+// const arr = [2, 4, 7, 8];
+// let firstOdd = arr.find((num) => num % 2 === 1);
+// console.log(firstOdd);
+
+// array-4
+
+// const arr = [5, 10, 15];
+// let sum = arr.reduce((total, num) => total + num, 0);
+// console.log(sum);
+
+// array-5
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// let evenNumber = arr.filter((num) => num % 2 === 0);
+// console.log(evenNumber);
+
+// string-1
+
+// let str = "How are you doing today?";
+// console.log(str.split(" ").join(","));
+// console.log(str.split("").join(","));
+
+// string-2
+
+// let str = "Today lesson was good!";
+// console.log(str.replaceAll("o", "w"));
+
+// string-3
+
+// let str = "We are learning JavaScript";
+// console.log(str.slice(7, 15));
+
+// string-4
+
+// let str = "Is JavaScript easy?";
+// const unli = ["a", "e", "i", "o", "u"];
+// let count = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase();
+//     if (unli.includes(char)) {
+//         count++;
+//     }
+// }
+
+// console.log(`${count} ta unli harf bor`);
+
+// string-5
+
+// let str = "We love JavaScript";
+// let changedWord = str.split(" ").reverse().join(" ");
+// console.log(changedWord);
