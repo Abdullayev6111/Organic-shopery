dd1d0fdf2d4f4b1c452e1deac8841859ff9355c5
Abdurauf Abdullayev <abdullayevaxmadullo977@gmail.com>
2025-09-21 23:51:40 +0500

    cat

--- script.js
+++ script.js
@@ -1,67 +1,68 @@
-// array-1
+// Student Management System
 
-// const arr1 = [1, 2];
-// const arr2 = [3, 4];
+// class Student {
+//     constructor(id, name, grades) {
+//         this.id = id;
+//         this.name = name;
+//         this.grades = grades;
+//     }
+//     addGrade(grade) {
+//         this.grades.push(grade);
+//     }
+//     getAverage() {
+//         const sum = this.grades.reduce((total, num) => total + num, 0);
+//         return sum / this.grades.length;
+//     }
+// }
+// const user = new Student(1, "Abu", [90, 80, 85]);
 
-// const mixedArray = arr1.concat(arr2);
-// console.log(mixedArray);
+// user.addGrade(95);
+// console.log(user.getAverage());
 
-// array-2
+// Shape (Inheritance + Polymorphism)
 
-// const arr = [3, 1, 4, 2];
-// console.log(arr.sort((a, b) => a - b));
-
-// array-3
-
-// const arr = [2, 4, 7, 8];
-// let firstOdd = arr.find((num) => num % 2 === 1);
-// console.log(firstOdd);
-
-// array-4
-
-// const arr = [5, 10, 15];
-// let sum = arr.reduce((total, num) => total + num, 0);
-// console.log(sum);
-
-// array-5
-
-// const arr = [1, 2, 3, 4, 5, 6];
-// let evenNumber = arr.filter((num) => num % 2 === 0);
-// console.log(evenNumber);
-
-// string-1
-
-// let str = "How are you doing today?";
-// console.log(str.split(" ").join(","));
-// console.log(str.split("").join(","));
-
-// string-2
-
-// let str = "Today lesson was good!";
-// console.log(str.replaceAll("o", "w"));
-
-// string-3
-
-// let str = "We are learning JavaScript";
-// console.log(str.slice(7, 15));
-
-// string-4
-
-// let str = "Is JavaScript easy?";
-// const unli = ["a", "e", "i", "o", "u"];
-// let count = 0;
-
-// for (let i = 0; i < str.length; i++) {
-//     let char = str[i].toLowerCase();
-//     if (unli.includes(char)) {
-//         count++;
+// class Shape {
+//     area() {
+//         return 0;
 //     }
 // }
 
-// console.log(`${count} ta unli harf bor`);
+// class Circle extends Shape {
+//     constructor(r) {
+//         super();
+//         this.r = r;
+//     }
+//     area() {
+//         return Math.floor(Math.PI * this.r ** 2);
+//     }
+// }
 
-// string-5
+// class Square extends Shape {
+//     constructor(a) {
+//         super();
+//         this.a = a;
+//     }
+//     area() {
+//         return this.a * this.a;
+//     }
+// }
 
-// let str = "We love JavaScript";
-// let changedWord = str.split(" ").reverse().join(" ");
-// console.log(changedWord);
+// class Rectangle extends Shape {
+//     constructor(a, b) {
+//         super();
+//         this.a = a;
+//         this.b = b;
+//     }
+//     area() {
+//         return this.a * this.b;
+//     }
+// }
+
+// const circ = new Circle(5);
+// console.log(circ.area());
+
+// const sqr = new Square(4);
+// console.log(sqr.area());
+
+// const rec = new Rectangle(4, 6);
+// console.log(rec.area());
