30d17a7b0d1018e75bfaf6035491ac6b6effddd9
Abdurauf Abdullayev <abdullayevaxmadullo977@gmail.com>
2025-09-22 22:20:15 +0500

    cat

--- script.js
+++ script.js
@@ -66,3 +66,79 @@
 
 // const rec = new Rectangle(4, 6);
 // console.log(rec.area());
+
+$(document).ready(function () {
+    const $thumbnails = $(".gallery-thumbs img");
+    const $mainImage = $("#mainProductImage");
+    const $prevBtn = $("#prevImage");
+    const $nextBtn = $("#nextImage");
+    let currentIndex = 0;
+    const totalImages = $thumbnails.length;
+
+    if ($thumbnails.length === 0) {
+        console.error("Thumbnail rasmlar topilmadi!");
+        return;
+    }
+
+    $thumbnails.first().addClass("active");
+
+    $thumbnails.on("click", function (e) {
+        e.preventDefault();
+        const index = parseInt($(this).data("index"));
+
+        if (index !== currentIndex) {
+            updateImage(index);
+        }
+    });
+
+    if ($prevBtn.length) {
+        $prevBtn.on("click", function (e) {
+            e.preventDefault();
+            const newIndex =
+                currentIndex > 0 ? currentIndex - 1 : totalImages - 1;
+            updateImage(newIndex);
+        });
+    }
+
+    if ($nextBtn.length) {
+        $nextBtn.on("click", function (e) {
+            e.preventDefault();
+            const newIndex =
+                currentIndex < totalImages - 1 ? currentIndex + 1 : 0;
+            updateImage(newIndex);
+        });
+    }
+
+    function updateImage(index) {
+        console.log("Rasm o'zgartirilmoqda:", index);
+
+        $thumbnails.removeClass("active");
+
+        $thumbnails.eq(index).addClass("active");
+
+        const newImageSrc =
+            $thumbnails.eq(index).data("large") ||
+            $thumbnails.eq(index).attr("src");
+
+        $mainImage.fadeOut(200, function () {
+            $mainImage.attr("src", newImageSrc).fadeIn(200);
+        });
+
+        currentIndex = index;
+    }
+
+    $(document).on("keydown", function (e) {
+        if ($(".product-gallery").is(":visible")) {
+            switch (e.keyCode) {
+                case 37:
+                    if ($prevBtn.length) $prevBtn.click();
+                    break;
+                case 39:
+                    if ($nextBtn.length) $nextBtn.click();
+                    break;
+            }
+        }
+    });
+
+    console.log("Gallery initialized:", { totalImages, currentIndex });
+});
